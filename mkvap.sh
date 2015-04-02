a50da5a2d8e3864b5428ae2767dc20dfdb19b348
poopyTo <chan.mike@hotmail.com>
2015-04-01 22:51:27 -0400

    Can hold a connection, but still something weird with forwarding data..

--- mkvap.sh
+++ mkvap.sh
@@ -4,39 +4,34 @@
 sudo apt-get install hostapd isc-dhcp-server
 
 # Configure dhcp server
-if ! grep -q "subnet 10.10.0.0 netmask 255.255.255.0 {
-range 10.10.0.2 10.10.0.16;
-option domain-name-servers 8.8.4.4, 208.67.222.222;
-options routers 10.10.0.1;
+if ! grep -q "subnet 10.0.0.0 netmask 255.255.255.0 {
+option routers 10.0.0.1;
+option domain-name-servers 8.8.8.8, 8.8.4.4, 208.67.222.222;
+range 10.0.0.2 10.0.0.16;
 }" /etc/dhcp/dhcpd.conf ; then
-sudo sh -c "echo 'subnet 10.10.0.0 netmask 255.255.255.0 {
-range 10.10.0.2 10.10.0.16;
-option domain-name-servers 8.8.4.4, 208.67.222.222;
-options routers 10.10.0.1;
+sudo sh -c "echo 'subnet 10.0.0.0 netmask 255.255.255.0 {
+option routers 10.0.0.1;
+option domain-name-servers 8.8.8.8, 8.8.4.4, 208.67.222.222;
+range 10.0.0.2 10.0.0.16;
 }' >> /etc/dhcp/dhcpd.conf"
 fi
+
+# Allow dhcp server to run for our ap iface
+if grep -q "INTERFACES=" /etc/default/isc-dhcp-server ; then
+	sudo sed -i '/INTERFACES=/d' /etc/default/isc-dhcp-server
+fi
+sudo sh -c "echo 'INTERFACES=\"$1\"' >> /etc/default/isc-dhcp-server"
 
 # Configure static IP for the AP
 if ! grep -q "auto $1
 iface $1 inet static
-hostapd /etc/hostapd/hostapd.conf
 address 10.0.0.1
 netmask 255.255.255.0" /etc/network/interfaces ; then
 sudo sh -c "echo 'auto $1
 iface $1 inet static
-hostapd /etc/hostapd/hostapd.conf
 address 10.0.0.1
 netmask 255.255.255.0' >> /etc/network/interfaces"
 fi
-
-# Restart the DHCP server and route interface
-sudo nmcli nm wifi off
-sudo rfkill unblock wlan
-sudo ifconfig $1 10.0.0.1 netmask 255.255.255.0 up # Init the interface
-sleep 1
-sudo /etc/init.d/isc-dhcp-server restart # Restart dhcp server
-sudo sysctl -w net.ipv4.ip_forward=1 # Allow IP forwarding
-sudo iptables -t nat -A POSTROUTING -s 10.10.0.0/16 -o $2 -j MASQUERADE # Routing rule
 
 # Configure hostapd
 if [ -f /etc/hostapd/hostapd.conf ]; then # if config exists, remove
@@ -50,13 +45,22 @@ hw_mode=g
 channel=11' >> /etc/hostapd/hostapd.conf"
 
 # Configure the hostapd daemon
-if ! grep -q "/etc/hostapd/hostapd.conf" /etc/default/hostapd ; then
-	sudo sh -c "echo 'DAEMON_CONF='/etc/hostapd/hostapd.conf'' >> /etc/default/hostapd"
+if grep -q "DAEMON_CONF=" /etc/default/hostapd ; then
+	sudo sed -i '/DAEMON_CONF=/d' /etc/default/hostapd
 fi
+sudo sh -c "echo 'DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"' >> /etc/default/hostapd"
+
+# Restart the DHCP server and route interface
+sudo nmcli nm wifi off
+sudo rfkill unblock wlan
+sudo ifconfig $1 10.0.0.1 netmask 255.255.255.0 up # Init the interface
+sleep 2
+sudo /etc/init.d/isc-dhcp-server restart # Restart dhcp server
+sudo sysctl -w net.ipv4.ip_forward=1 # Allow IP forwarding
+sudo iptables -t nat -A POSTROUTING -s 10.0.0.0/16 -o $2 -j MASQUERADE # Routing rule
 
 # Finally, launch the AP
-sudo service isc-dhcp-server restart
 #sudo hostapd -d /etc/hostapd/hostapd.conf
 sudo service hostapd start
-#sudo nmcli nm wifi on
-#sudo service hostapd restart
+sudo nmcli nm wifi on
+sudo service hostapd restart	
