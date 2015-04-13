4cfb1f50b04c494d73c1c03c53ec0ee6a5031e18
Nigel Mason <ndmason@live.ca>
2015-04-13 16:40:17 -0400

    clean up old code/scripts

--- mktap.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-ip tuntap add tap0 mode tap
-ip addr add 192.168.5.10/24 dev tap0 # TODO get IP from args
-ip link set tap0 up
-echo "interface tap0 created"
--- rmtap.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-ip tuntap del tap0 mode tap
-echo "interface tap0 destroyed"
